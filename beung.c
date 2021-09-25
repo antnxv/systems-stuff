202a3085e7d15bed2349e02f4b2197b37789cc03
anosaryev <anthonynos@outlook.com>
2021-09-25 01:25:26 -0400

    updated with (slow, probably sub-optimal) problem 5 solution

--- beung.c
+++ beung.c
@@ -12,6 +12,32 @@ int multi35(){ // Problem 1
   return s;
 }
 
+/*
+ * m = 10: returns 2520;
+ * m = 20: returns 232792560
+ * It's a bit slow, but the only types of
+ * optimisation I can think of require
+ * arrays and I'm too scared to delve into
+ * those for now.
+*/
+int allFactors(){ // Problem 5
+  int i;
+  int m = 20;
+  int n = 1;
+  int b = 0;
+  while (!b){
+    for (i = 1; i <= m; i++){
+      if (n%i != 0){
+        n++;
+        b--;
+        break;
+      }
+    }
+    b++;
+  }
+  return n;
+}
+
 int sumSquareDiff(){ // Problem 6
   int uq = 0;
   int qu = 0;
@@ -24,5 +50,7 @@ int sumSquareDiff(){ // Problem 6
 }
 
 int main(){
-
+  printf("Problem 1 Solution: %d\n", multi35());
+  printf("Problem 5 Solution: %d\n", allFactors());
+  printf("Problem 6 Solution: %d\n", sumSquareDiff());
 }
