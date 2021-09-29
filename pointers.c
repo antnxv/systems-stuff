6ed10e1246fd557a8be86e7ea8bbde36f7975574
anosaryev <anthonynos@outlook.com>
2021-09-28 23:23:56 -0400

    printed addresses of each variable in hex and decimal

--- pointers.c
+++ pointers.c
@@ -4,4 +4,7 @@ int main(){
   int i = 29519;
   char c = 'Q';
   long o = 6273845268835124;
+  printf("\nint i = %d\n\t%p\n\t%lu\n", i, &i, (long unsigned) &i);
+  printf("\nchar c = %c\n\t%p\n\t%lu\n", c, &c, (long unsigned) &c);
+  printf("\nlong o = %ld\n\t%p\n\t%lu\n", o, &o, (long unsigned) &o);
 }
