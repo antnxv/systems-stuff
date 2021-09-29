203670fe267f046acd6662e7e866d589b882bf02
anosaryev <anthonynos@outlook.com>
2021-09-28 23:30:14 -0400

    declared and initialised pointers, printed values

--- pointers.c
+++ pointers.c
@@ -7,4 +7,8 @@ int main(){
   printf("\nint i = %d\n\t%p\n\t%lu\n", i, &i, (long unsigned) &i);
   printf("\nchar c = %c\n\t%p\n\t%lu\n", c, &c, (long unsigned) &c);
   printf("\nlong o = %ld\n\t%p\n\t%lu\n", o, &o, (long unsigned) &o);
+  int *ip = &i;
+  char *cp = &c;
+  long *ldp = &o;
+  printf("\nip = %p\ncp = %p\nldp = %p\n", ip, cp, ldp);
 }
