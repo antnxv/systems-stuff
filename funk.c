aac7f6ef5d09318f576588a33c744d62be951f5e
anosaryev <anthonynos@outlook.com>
2021-10-04 21:10:25 -0400

    finished readray

--- funk.c
+++ funk.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
-int readray(int *ray) {
+int readray(int *ray, int size) {
+  int i;
+  printf("[ ");
+  for (i = 0; i < size; i ++){
+    printf("%d ", ray[i]);
+  }
+  printf("]\n");
   return 0;
 }
 
-int avg(int *ray) {
+int avg(int *ray, int size) {
   return 0;
 }
 
-int copy2(int *a, int *b){
+int copy2(int *a, int *b, int size){
   return 0;
 }
 
