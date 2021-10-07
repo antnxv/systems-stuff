b85b6edfd40c022316ba7a5d76c0cbb210b7318a
anosaryev <anthonynos@outlook.com>
2021-10-07 04:40:29 -0400

    added final check to mystrcmp

--- mystring.c
+++ mystring.c
@@ -62,7 +62,7 @@ int mystrcmp(char *s1, char *s2) {
     if (s1[i] != s2[i])
       return s1[i] - s2[i];
   }
-  return 0;
+  return s1[i] - s2[i];
 }
 
 char * mystrchr(char *s, char c) {
@@ -74,31 +74,3 @@ char * mystrchr(char *s, char c) {
   }
   return s+i;
 }
-/*
-int main() {
-  char s1[100];
-  char s2[100] = "hello";
-  printf("comp: %d", mystrcmp(mystrcpy(s1, s2), strcpy(s1, s2)));
-  return 0;
-}
-
-int main() {
-
-  char s1[100];
-  char s2[100] = "hello";
-  char s3[100] = "goodbye";
-
-  printf("start strings:\n");
-  printf("s1: [%s]\n", s1);
-  printf("s2: [%s]\n", s2);
-  printf("s3: [%s]\n", s3);
-
-  printf("\nTesting strlen(s2):\n");
-  printf("[standard]:\t%lu\n", strlen(s2) );
-  printf("[mine]:\t\t%d\n", mystrlen(s2) );
-
-  printf("\nTesting strcpy(s1, s2):\n");
-  printf("[standard]:\t[%s]\n", strcpy(s1, s2) );
-  printf("[mine]:\t\t[%s]\n", mystrcpy(s1, s2) );
-}
-*/
