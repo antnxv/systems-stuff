b78df6267625c8657c7345a3eb7b6922ace99056
anosaryev <anthonynos@outlook.com>
2021-10-15 02:01:59 -0400

    included more libraries, updated headers

--- structy.h
+++ structy.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct meau {char *n; int h; char *u;};
+struct meau {char *n; float h; char u;};
 
 void print_meau(struct meau *m);
-struct meau * make_meau(char *n, int h, char *u);
-struct meau * swap_meau(struct meau *m);
+struct meau * make_meau(char *n, float h, char u);
+struct meau * swap_meau(struct meau *m, char nu);
