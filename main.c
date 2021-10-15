4c3f4d0059db2e32d7562b5fcfcf02f64bfe9626
anosaryev <anthonynos@outlook.com>
2021-10-15 02:02:46 -0400

    lightly tested further features

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include "structy.h"
 
 int main(){
-  struct meau jenny;
-  jenny.n = "jenny";
-  jenny.h = 170;
-  jenny.u = "cm";
-  print_meau(&jenny);
+  struct meau *jenny;
+  struct meau *axel;
+  
+  jenny = make_meau("jenny", 170, 'c');
+  print_meau(jenny);
+
+  jenny = swap_meau(jenny, 'i');
+  print_meau(jenny);
+  
+  free(jenny);
+
+  printf("\n");
+  
+  axel = make_meau("axel", 67.5, 'i');
+  print_meau(axel);
+
+  axel = swap_meau(axel, 'c');
+  print_meau(axel);
+  
+  free(axel);
+
+
   return 0;
 }
