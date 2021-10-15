d69ecabb27c4e988d82fc40600c300098a608369
anosaryev <34826608+anosaryev@users.noreply.github.com>
2021-10-15 11:18:58 -0400

    implemented randomisation of test case
    apologies for doing this during class time, i forgot to do this at home

--- main.c
+++ main.c
@@ -1,26 +1,16 @@
 #include "structy.h"
+#include <time.h>
 
 int main(){
   struct meau *jenny;
-  struct meau *axel;
+  srand(time(NULL));
   
-  jenny = make_meau("jenny", 170, 'c');
-  print_meau(jenny);
+  jenny = make_meau("jenny", rand()%200, 'c'+(rand()%2)*6);
 
   jenny = swap_meau(jenny, 'i');
   print_meau(jenny);
   
   free(jenny);
-
-  printf("\n");
-  
-  axel = make_meau("axel", 67.5, 'i');
-  print_meau(axel);
-
-  axel = swap_meau(axel, 'c');
-  print_meau(axel);
-  
-  free(axel);
 
 
   return 0;
