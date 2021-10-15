51540827b7d1854eb4bcaee23c13a7a1a0b17024
anosaryev <anthonynos@outlook.com>
2021-10-15 01:03:24 -0400

    made print method

---
+++ structy.c
@@ -0,0 +1,13 @@
+#include "structy.h"
+
+void print_meau(struct meau *m){
+  printf("%s is %d %s tall\n", m->n, m->h, m->u);
+}
+
+struct meau * make_meau(char *n, int h, char *u){
+  
+}
+
+struct meau * swap_meau(struct meau *m){
+  
+}
