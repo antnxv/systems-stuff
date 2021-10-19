9642df9072b7b2be066119760e2e560f33121f08
anosaryev <anthonynos@outlook.com>
2021-10-19 01:04:42 -0400

    streamlined tests, included print_swap_list

--- main.c
+++ main.c
@@ -3,27 +3,19 @@
 int main(){
   srand(time(NULL));
 
-  struct node *jenny;
-  char unit = 'c' + (rand()%2)*6;
-  jenny = make_node("jenny", ((unit == 'c')? 100+rand()%100 : 39+rand()%40), unit);
-  struct node *axel;
-  unit = 'c' + (rand()%2)*6;
-  axel = insert_front(jenny, "axel", ((unit == 'c')? 100+rand()%100 : 39+rand()%40), unit);
-  struct node *mandy;
-  unit = 'c' + (rand()%2)*6;
-  mandy = insert_front(axel, "mandy", ((unit == 'c')? 100+rand()%100 : 39+rand()%40), unit);
-
-  struct node *m1 = mandy;
-  while (m1){
-    m1 = swap_unit(m1, (m1->unit == 'c')? 'i' : 'c');
-    print_list(m1);
-
-    m1 = swap_unit(m1, (m1->unit == 'c')? 'i' : 'c');
-    print_list(m1);
-
-    m1 = m1->next;
+  struct node *folks = 0;
+  char names[5][10] = {"jenny", "axel", "mandy", "niko", "maple"};
+  int i;
+  
+  for (i = 0; i < 5; i ++){
+    char unit = 'c' + (rand() % 2) * 6; // randomises unit between 'c' and 'i'
+    int tall = (unit == 'c')? 100 + rand() % 100 : 39 + rand() % 40; // randomises height
+    folks = insert_front(folks, names[i], tall, unit);
   }
+  
+  //print_list(folks); // lame version
+  print_swap_list(folks); // cool version
 
-  printf("\nFirst node after running free_list: %p\n", free_list(jenny));
+  printf("\nFirst node after running free_list: %p\n", free_list(folks));
   return 0;
 }
