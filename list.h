25588ba15857502cc362381bb010a0312112c47b
anosaryev <anthonynos@outlook.com>
2021-10-19 01:04:07 -0400

    added print_swap_list and remove_node methods

--- list.h
+++ list.h
@@ -6,7 +6,9 @@
 struct node {char *name; float tall; char unit; struct node *next;};
 
 void print_list(struct node *m);
+void print_swap_list(struct node *m);
 struct node *make_node(char *name, float tall, char unit);
 struct node *insert_front(struct node *m1, char *name, float tall, char unit);
+struct node *remove_node(struct node *m1, int data);
 struct node *free_list(struct node *m1);
 struct node *swap_unit(struct node *m, char newnit);
