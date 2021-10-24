f3fa6db0d9f0ef5a1ee26548a733c83812d38800
anosaryev <anthonynos@outlook.com>
2021-10-24 05:12:20 -0400

    added more test cases, tested random and changed remove testing

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "list.h"
 
 int main(){
+  srand(time(0)); //temporarily in main
+  
   struct song *list = 0;
   list = insert(list, "Blood Type", "Kino");
   list = insert(list, "Mama, We've All Gone Mad", "Kino");
@@ -8,11 +10,14 @@ int main(){
   list = insert(list, "False", "Kiwi");
   list = insert(list, "Onward, We'll Proceed", "Kino");
   list = insert(list, "War", "Kino");
+  list = insert(list, "O", "K");
   list = insert(list, "Test", "Kinz");
   list = insert(list, "Exam", "Kina");
+  list = insert(list, "Blobble", "Kinog");
   list = insert(list, "The Slacker", "Kino");
   list = insert(list, "It's Not That Bad", "Kane Strang");
   list = insert(list, "My Friends", "Kino");
+  list = insert(list, "Bumboop", "Kin");
   list = insert(list, "Troonkvihlihzator", "Kino");
   list = insert(list, "Every Night", "Kino");
   list = insert(list, "Uzbekistan", "Kyrgiz");
@@ -21,25 +26,34 @@ int main(){
 
   print_list(list);
   printf("\n");
-  
+
   struct song *found;
-  
+
   found = search_song(list, "Not", "Kino");
   printf("%p\t%s = %s\n", found, found->name, "0");
-  
+
   found = search_song(list, "My Friends", "Kino");
   printf("%p\t%s = %s\n", found, found->name, "My Friends");
 
   found = search_artist(list, "Kino");
   printf("%p\t%s = %s\n", found, found->name, "Blood Type");
-  
-  found = search_artist(list, "Kyrgiz");
-  printf("%p\t%s = %s\n", found, found->name, "Uzbekistan"); 
-  
-  while (search_artist(list, "Kino"))
-    remove_song(list, search_artist(list, "Kino")->name, "Kino");
 
+  found = search_artist(list, "Kyrgiz");
+  printf("%p\t%s = %s\n", found, found->name, "Uzbekistan");
+
+  printf("\n");
+  int z;
+  for (z = 0; z < 10; z ++){
+    found = random_song(list);
+    print_song(found);
+  }
+  
+  printf("\n");
+  while (search_artist(list, "Kino"))
+    list = remove_song(list, search_artist(list, "Kino")->name, "Kino");
   print_list(list);
 
-  free_list(list);
+  printf("\n");
+  list = free_list(list);
+  print_list(list);
 }
