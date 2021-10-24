91a264950136964b0e5659133aeccc98e3b917bf
anosaryev <anthonynos@outlook.com>
2021-10-24 05:31:32 -0400

    dumped headers into a file

---
+++ librayray.c
@@ -0,0 +1,34 @@
+#include "librayray.h"
+
+srand(time(0));
+
+struct song **make_library(){
+  
+}
+
+struct song **add_song(struct song *song){
+}
+
+struct song *search_songl(char *name, char *artist){
+}
+
+struct song *search_artistl(char *artist){
+}
+
+void print_letter(struct song **song){
+}
+
+void print_artist(struct song **song){
+}
+
+void print_library(struct song **song){
+}
+
+void print_shuffle(struct song **song){
+}
+
+struct song **remove_songl(char *name, char *artist){
+}
+
+struct song **free_library(struct song **song){
+}
