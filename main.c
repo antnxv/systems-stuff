6f322fbd5a6476baaba88f7b83483f65e5d6389c
anosaryev <anthonynos@outlook.com>
2021-11-03 17:30:01 -0400

    switched allFactors() to returning long

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 
 int main(){
   printf("Problem 1 Solution: %d\n", multi35());
-  printf("Problem 5 Solution: %d\n", allFactors());
+  printf("Problem 5 Solution: %ld\n", allFactors());
   printf("Problem 6 Solution: %d\n", sumSquareDiff());
 }
