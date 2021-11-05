190996c9ab564654c8beed5e5b758866c1705cd5
anosaryev <anthonynos@outlook.com>
2021-11-05 00:39:24 -0400

    added command line argument directory requirement, fixed file type display error, implemented general improvements here and there; apologies for this being late, i did not know this portion of the assignment was due yesterday because the stuycs.org page did not say it was until class yesterday

--- dirinfo.c
+++ dirinfo.c
@@ -7,40 +7,52 @@
 
 struct size{long s; char p;};
 
-long unsigned size(DIR *d){
-  int s, si = 0;
-  struct dirent *ent;
-  struct stat st;
-  
-  while (ent = readdir(d)){
-    if (stat(ent->d_name, &st)){
-      printf("Error %d: %s\n", errno, strerror(errno));
-    }
-    s += st.st_size;
-  }
-  
-  rewinddir(d);
-  return s;
-}
-
 int prefix(long unsigned s){
   if (s > 1000000000){
     return 'G';
   }else if (s > 1000000){
     return 'M';
-  }else if(s > 1000){
+  }else if (s > 1000){
     return 'K';
   }
   return 0;
 }
 
-int printdir(DIR *d){
+char *path(char *dir, char *pa, char *name){
+  strcpy(pa, dir);
+  strcat(pa, name);
+  return pa;
+}
+
+long unsigned size(char *pa, DIR *d, char *dir){
+  int s, si = 0;
+  struct dirent *ent;
+  struct stat st;
+
+  while (ent = readdir(d)){
+    if (stat(path(dir, pa, ent->d_name), &st) < 0){
+      printf("Error %d: %s\n", errno, strerror(errno));
+      printf("\tFaulty Path: %s\n", pa);
+    }
+    s += st.st_size;
+  }
+
+  rewinddir(d);
+  return s;
+}
+
+int printdir(DIR *d, char *dir){
   struct dirent *ent;
   struct stat st;
 
   while (ent = readdir(d)){
     if (ent->d_type == DT_DIR){
-      stat(ent->d_name, &st);
+      char pa[strlen(dir)];
+      if (stat(path(dir, pa, ent->d_name), &st) < 0){
+	printf("Error %d: %s\n", errno, strerror(errno));
+	printf("\tFaulty Path: %s\n", pa);
+	return -1;
+      }
       long unsigned s = st.st_size;
       char p = prefix(s);
       switch (p){
@@ -48,7 +60,7 @@ int printdir(DIR *d){
       case 'M': s /= 1000;
       case 'K': s /= 1000;
       }
-      printf("File:\t%07o\t%lu %cB\t%s\n", st.st_mode, s, p, ent->d_name);
+      printf("Dir:\t%07o\t%lu %cB\t%s\n", st.st_mode, s, p, ent->d_name);
     }
   }
 
@@ -56,13 +68,18 @@ int printdir(DIR *d){
   return 0;
 }
 
-int printfil(DIR *d){
+int printfil(DIR *d, char *dir){
   struct dirent *ent;
   struct stat st;
 
   while (ent = readdir(d)){
     if (ent->d_type != DT_DIR){
-      stat(ent->d_name, &st);
+      char pa[strlen(dir)];
+      if (stat(path(dir, pa, ent->d_name), &st) < 0){
+	printf("Error %d: %s\n", errno, strerror(errno));
+	printf("\tFaulty Path: %s\n", pa);
+	return -1;
+      }
       long unsigned s = st.st_size;
       char p = prefix(s);
       switch (p){
@@ -78,15 +95,28 @@ int printfil(DIR *d){
   return 0;
 }
 
-int main(){
-  DIR *d;
-  d = opendir(".");
-
-  if (d < 0){
-    printf("Error %d: %s\n", errno, strerror(errno));
+int main(int argc, char *argv[]){
+  if (argc == 1){
+    printf("Enter a directory to scan\n");
+    return -1;
   }
-
-  long unsigned s = size(d);
+  
+  int i;
+  char dir[strlen(argv[1])+1];
+  strcpy(dir, argv[1]);
+  if (dir[strlen(dir)-1] != '/')
+    strcat(dir, "/");
+  
+  DIR *d;
+  d = opendir(dir);
+  if (d == 0){
+    printf("Error %d: %s\n", errno, strerror(errno));
+    return -1;
+  }
+  
+  char pa[strlen(dir)+101];
+  long unsigned s = size(pa, d, dir);
+  
   char p = prefix(s);
   switch (p){
   case 'G': s /= 1000;
@@ -95,8 +125,9 @@ int main(){
   }
 
   printf("Total Directory Size: %lu %cB\n", s, p);
-  printdir(d);
-  printfil(d);
-  
+  printdir(d, dir);
+  printfil(d, dir);
+
   closedir(d);
+  return 0;
 }
